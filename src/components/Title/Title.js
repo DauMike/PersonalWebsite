b2a76debe40a600b6cafa17adb4368c7184a40a6
DauMike <migueldautant17@gmail.com>
2024-01-05 12:10:43 -0500

    Starting

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+  return (
+    <div className='Title'>
+      <h1>Miguel Dautant</h1>
+    </div>
+  )
+}
+
+export default Title
